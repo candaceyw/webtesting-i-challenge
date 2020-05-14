f3a96e07606f0611501d072004d5f0f33f300859
candaceyw <candaceyw@gmail.com>
2020-05-14 15:03:30 -0400

    MVP with some strech

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,32 +1,20 @@
 module.exports = {
-	succeed,
-	fail,
-	repair,
-	get,
+  succeed,
+  fail,
+  repair,
+  get,
 };
 
 // method that accepts an item object and returns a
 // new item object modified according to the rules
 // defined by the client for enhancement success.
 function succeed(item) {
-	if (item.enhancement === 20) {
-		return { ...item };
-	} else {
-		return { ...item, enhancement: item.enhancement + 1 };
-	}
+  if (item.enhancement === 20) {
+    return { ...item };
+  } else {
+    return { ...item, enhancement: item.enhancement + 1 };
+  }
 }
-
-// function succeed(item) {
-// 	const minEhancement = 0;
-// 	const maxEnhancement = 20;
-// 	if (item.enhancement < maxEnhancement) {
-// 		item.enhancement++;
-// 	}
-// 	if (item.enhancement < minEhancement) {
-// 		item.enhancement = 0;
-// 	}
-// 	return { ...item };
-// }
 
 /**
    * If the item's enhancement is less than 15, the durability of the item is decreased by 5.
@@ -34,28 +22,34 @@ function succeed(item) {
 - If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
    */
 function fail(item) {
-	if (item.enhancement < 15) {
-		return { ...item, durability: item.durability - 5 };
-	} else if (item.enhancement >= 15 && item.enhancement < 17) {
-		return { ...item, durability: item.durability - 10 };
-	} else {
-		return { ...item, durability: item.durability - 1 };
-	}
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.enhancement >= 15 && item.enhancement < 17) {
+    return { ...item, durability: item.durability - 10 };
+  } else {
+    return { ...item, durability: item.durability - 1 };
+  }
 }
 
 function repair(item) {
-	return { ...item, durability: 100 };
+  return { ...item, durability: 100 };
 }
 
-// method for use when working on the stretch problem.
-// method to the enhancer object that takes an item and returns
-// a new item with the name property modified according to the following rules:
+//STRETCH
 
 // if the enhancement level is 0, the the name is not modified.
 // if the enhancement level is greater than 0, change the name to include the
 //  enhancement level, preceded by a plus sign ( + ), between square brackets
-//   before the item's name. Example: the name of a "Iron Sword" enhanced to 7 would
-//   be "[+7] Iron Sword".
+//   before the item's name.
+
 function get(item) {
-	return { ...item };
+  // if the enhancement level is 0, the the name is not modified.
+  if (item.enhancement === 0) {
+    return { ...item };
+  } else {
+    if (item.enhancement > 0) {
+      item.name = `[+${item.enhancement}] ${item.name}`;
+    }
+    return { ...item };
+  }
 }
